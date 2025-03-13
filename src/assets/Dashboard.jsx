c96877be1ae776ac970170bff36bb22c87177441
Teamfrontend <varshini021nexether@gmail.com>
2025-03-13 11:16:11 +0530

    first commit

--- src/assets/Dashboard.jsx
+++ src/assets/Dashboard.jsx
@@ -15,16 +15,6 @@ export default function Dashboard() {
           <ul className="nav-list">
             <li>Dashboard</li>
             <li>Form Elements</li>
-            <div class="dropdown">
-  <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown
-  </button>
-  <ul class="dropdown-menu">
-    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
-    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
-    <li><button class="dropdown-item" type="button">Dropdown item</button></li>
-  </ul>
-</div>
             <li>Charts</li>
             <li>Investment Analysis</li>
             <li>Marketing Overview</li>
